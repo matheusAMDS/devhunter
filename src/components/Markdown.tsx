16ed48b5274fb9d12ebc49490f88c95502947b88
matheusAMDS <matheusa56@gmail.com>
2021-04-18 01:38:45 -0300

    feature: render a html link for markdown links in job details

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown"
 import ChakraUIRenderer, { defaults } from "chakra-ui-markdown-renderer"
-import { Heading } from "@chakra-ui/layout"
+import { Heading, Link } from "@chakra-ui/layout"
+import gfm from "remark-gfm"
 
 interface Props {
   src: string
@@ -15,10 +16,21 @@ const Markdown: React.FC<Props> = ({ src }) => {
           const { children, level } = props
           
           return <Heading size="lg" my={4}>{children}</Heading>
+        },
+        link: props => {
+          console.log(props)
+          const { children, href } = props 
+
+          return (
+            <Link href={href} target="_blank" color="blue.400">
+              {children}
+            </Link>
+          )
         }
       })}
       source={src}
       escapeHtml={false}
+      plugins={[gfm]}
     />
   )
 }
