02bde0b4924faac068210a8c05aa25d4adf166b1
matheusAMDS <matheusa56@gmail.com>
2021-05-04 23:44:32 -0300

    add 'babel-plugin-transform-typescript-metadata' to write typegoose models more cleanly

--- src/lib/jobs/model.ts
+++ src/lib/jobs/model.ts
@@ -1,36 +1,35 @@
 import { getModelForClass, prop } from "@typegoose/typegoose"
 import { Base } from "@typegoose/typegoose/lib/defaultClasses"
-import { WhatIsIt } from "@typegoose/typegoose/lib/internal/constants"
 
 export class Job extends Base {
-  @prop({ type: () => String })
-  title: string 
+  @prop()
+  title: string
 
-  @prop({ type: () => String })
-  body: string 
+  @prop()
+  body: string
 
-  @prop({ type: () => String, required: false })
-  company?: string 
+  @prop({ required: false })
+  company?: string
 
-  @prop({ type: () => String })
-  location: string 
+  @prop()
+  location: string
 
-  @prop({ type: () => Boolean })
-  open: boolean 
+  @prop()
+  open: boolean
 
-  @prop({ type: () => String })
-  github_id: number 
+  @prop()
+  github_id: number
 
-  @prop({ type: () => String })
-  state: string 
+  @prop()
+  state: string
 
-  @prop({ type: () => Number, expires: 3888000 })
-  created_at: number 
+  @prop({ expires: 3888000 })
+  created_at: number
 
-  @prop({ type: () => Number })
-  updated_at: number  
+  @prop()
+  updated_at: number
 
-  @prop({ type: () => [String] }, WhatIsIt.ARRAY)
+  @prop({ type: () => [String] })
   labels: string[]
 }
 
